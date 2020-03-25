b66f7989db0ba00fb4319ac6a2ce16d6c4985435
LISA_COOK <lisabpink@gmail.com>
2020-03-25 18:27:56 -0400

    LDC fail

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -16,16 +16,38 @@ it("should run test true", function() {
 
 describe("success()", function() {
     it("should enhance item if lvl is not 20 succeed", function() {
-      let item = {};
-      item.enhancement = 0;
+        let item = { name: "Goddess Sword", enhancement: 2, durability: 50 };
       const enhanced = enhancer.succeed(item);
-      expect(enhanced.enhancement).toBe(1);
+      expect(enhanced.enhancement).toBe(3);
     });
     it("should not enhance item if lvl is 20 succeed", function() {
-      let item2 = {};
-      item2.enhancement = 20;
+      let item2 = {name: "Goddess Sword", enhancement: 20, durability: 50};
+      
       expect(() => {
         enhancer.succeed(item2);
       }).toThrow();
+    });
+    it("should not change durability", function() {
+        let item = { name: "Goddess Sword", enhancement: 2, durability: 50 };
+        const enhanced = enhancer.succeed(item);
+        expect(enhanced.durability).toBe(50);
+      });
+  });
+
+  describe("fail()", function() {
+    it("If the items enhancement is less than 15, the durability of the item is decreased by 5.", function() {
+      let item3 = { name: "Magic Wand", enhancement: 10, durability: 50 };
+      const fails = enhancer.fail(item3);
+      expect(fails.durability).toBe(45);
+    });
+    it("If the items enhancement is 15 or more, the durability of the item is decreased by 10", function() {
+        let item4 = { name: "Goddess Sword", enhancement: 15, durability: 50 };
+        const fails2 = enhancer.fail(item4);
+        expect(fails2.durability).toBe(40);
+    });
+    it("If the items enhancement is 15 or more, the durability of the item is decreased by 10", function() {
+      let item4 = { name: "Goddess Sword", enhancement: 17, durability: 50 };
+      const fails2 = enhancer.fail(item4);
+      expect(fails2.enhancement).toBe(16);
     });
   });
