02ac06dfce28434a2049f88449f8124e13860d92
LISA_COOK <lisabpink@gmail.com>
2020-03-25 18:11:21 -0400

    LDC repair

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,15 @@
-const enhancer = require('./enhancer.js');
+const enhancer = require("./enhancer.js");
 // test away!
+it("should run test true", function() {
+    expect(true).toBe(true);
+  });
+  
+  describe("enhacer.js", function() {
+    describe("repair()", function() {
+      it("should return item durability as 100", function() {
+        let item = {};
+        const fixed = enhancer.repair(item);
+        expect(fixed.durability).toBe(100);
+      });
+    });
+});
